59fe3590219986b4f54c8cafc868add7b98fd767
Taein <taein.k0103@gmail.com>
2024-12-01 19:08:26 +0900

    WebSocket Messages

--- clone_zoom/src/public/js/app.js
+++ clone_zoom/src/public/js/app.js
@@ -1,2 +1,19 @@
 //프런트와 백엔드 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+//이벤트 처리
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New messages: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- clone_zoom/src/server.js
+++ clone_zoom/src/server.js
@@ -15,9 +15,14 @@ const server = http.createServer(app); //http server
 const wss = new WebSocket.Server({ server }); //websocket server (on top of the http server)
 //{server} -> can run both http & websocket servers (on the same port)
 
-function handleConnection(socket) { console.log(socket) }
-
 //connection은 메서드로 socket을 받아서 저장해야 함, socket은 브라우저와의 연결
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser! ❌"));
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("hello!");
+});
 
 server.listen(3000, handleListen);
