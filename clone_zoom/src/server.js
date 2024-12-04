42e569f06b4d8602d2bce1fccf32ec40346a4321
Taein <taein.k0103@gmail.com>
2024-12-04 17:00:01 +0900

    Using SocketIO

--- clone_zoom/src/server.js
+++ clone_zoom/src/server.js
@@ -16,7 +16,12 @@ const httpServer = http.createServer(app); //http server
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
 })
 
 /*
