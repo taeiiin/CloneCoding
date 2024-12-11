a26241fda917723a3acf4d105a44b012ef9f93a3
Taein <taein.k0103@gmail.com>
2024-12-11 14:01:52 +0900

    User Count

--- clone_zoom/src/server.js
+++ clone_zoom/src/server.js
@@ -2,6 +2,7 @@ import http from "http";
 import express from "express";
 import SocketIO from "socket.io";
 import { copyFileSync } from "fs";
+import { count } from "console";
 //import WebSocket from "ws";
 
 const app = express();
@@ -29,6 +30,10 @@ function publicRooms() {
     return publicRooms;
 }
 
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
@@ -37,11 +42,11 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
     });
     socket.on("disconnect", () => {
         wsServer.sockets.emit("room_change", publicRooms());
